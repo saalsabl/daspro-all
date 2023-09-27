03d7ac9de5db65974e066f3ea573e78459ccff5b
saalsabl <aisyahnzkk@gmail.com>
2023-09-27 07:18:22 +0700

    modifikasi nilai tugas

--- jobsheet5/PemilihanPercobaan201.java
+++ jobsheet5/PemilihanPercobaan201.java
@@ -9,8 +9,8 @@ public class PemilihanPercobaan201 {
         System.out.print("Nilai kuis    : ");
         float kuis = input01.nextFloat();
         System.out.print("Nilai tugas   :" );
+        
         float tugas = input01.nextFloat();
-
         float total = (uas*0.4F) + (uts*0.3F) + (kuis * 0.1F) + (tugas*0.2F);
 
         String nilai;
