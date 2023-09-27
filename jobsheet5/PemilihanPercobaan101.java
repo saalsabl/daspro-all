ba74d949e70f53ea196f0d565104b894d09377d6
saalsabl <aisyahnzkk@gmail.com>
2023-09-27 07:02:55 +0700

    Ternary Operator

--- jobsheet5/PemilihanPercobaan101.java
+++ jobsheet5/PemilihanPercobaan101.java
@@ -4,9 +4,9 @@ public class PemilihanPercobaan101 {
         Scanner input01 =new Scanner(System.in);
         System.out.print("Masukkan angka: ");
         int angka = input01.nextInt();
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
+
+        String hasil;
+        hasil = (angka %2 == 0)? "bilangan genap" : "bilangan ganjil";
+        System.out.println(angka+ "termasuk" + hasil);
     }
-}
+} 
