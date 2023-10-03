175fd88bf8f407661bdf847b612bd345d89f9a55
saalsabl <aisyahnzkk@gmail.com>
2023-10-03 20:26:04 +0700

    modif2

--- jobsheet6/Pemilihan2Percobaan101.java
+++ jobsheet6/Pemilihan2Percobaan101.java
@@ -7,9 +7,15 @@ public class Pemilihan2Percobaan101 {
         System.out.print("Masukkan Tahun : ");
         int tahun = input01.nextInt();
 
-        if (((tahun % 4) == 0) && (tahun % 100!= 0) || (tahun%400 == 0)){
+        if ((tahun % 100) == 0) {
+            if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
-        } else
-            System.out.println("Bukan Tahun Kabisat");
+               else
+                if ((tahun % 400) == 0)
+                    System.out.println("Tahun Kabisat");
+                else 
+                     System.out.println("Bukan Tahun Kabisat");
+        }    else
+                 System.out.println("Bukan Tahun Kabisat");
     }
 }
