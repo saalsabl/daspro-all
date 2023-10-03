c02284fb49142e735c6e5396602021635b8cac3b
saalsabl <aisyahnzkk@gmail.com>
2023-10-03 20:40:43 +0700

    modif1

--- jobsheet6/Pemilihan2Percobaan201.java
+++ jobsheet6/Pemilihan2Percobaan201.java
@@ -16,9 +16,16 @@ public class Pemilihan2Percobaan201 {
 
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+                if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3)
+                    System.out.println("Segitiga tersebut adalah segitiga siku-siku sama kaki");
+                else
+                     System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else if (sudut1 == 60 && sudut2 == 60 && sudut3 == 60)
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut1 == sudut3)
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             else
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
         } else
                 System.out.println("Bukan Segitiga");
                 
