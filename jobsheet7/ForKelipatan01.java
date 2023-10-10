157c0b58048a8cee233c06a9bb27bb9cb87f3783
saalsabl <aisyahnzkk@gmail.com>
2023-10-10 16:37:19 +0700

    modif1

--- jobsheet7/ForKelipatan01.java
+++ jobsheet7/ForKelipatan01.java
@@ -3,7 +3,7 @@ package jobsheet7;
 public class ForKelipatan01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan;
+        int kelipatan, rataRata;
         int jumlah = 0;
         int counter = 0;
         int total = 0;
@@ -14,12 +14,14 @@ public class ForKelipatan01 {
             if (i % kelipatan == 0){
                 total +=i;
                 counter++;
-                System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+                
             }
 
         }
-
+        rataRata = jumlah / counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
     }
     
 }
