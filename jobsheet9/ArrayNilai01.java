f55bf506b6b4cdf10b369ec8f5f836cd3d839eef
saalsabl <aisyahnzkk@gmail.com>
2023-10-23 16:22:41 +0700

    percobaan2

---
+++ jobsheet9/ArrayNilai01.java
@@ -0,0 +1,18 @@
+package jobsheet9;
+
+import java.util.Scanner;
+
+public class ArrayNilai01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int[] nilaiAkhir = new int[10];
+        for(int i = 0; i < 10; i++){
+            System.out.print("Masukkan nilai akhir ke-"+ i + " : ");
+            nilaiAkhir[i] = scan.nextInt();
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+    }
+    
+}
