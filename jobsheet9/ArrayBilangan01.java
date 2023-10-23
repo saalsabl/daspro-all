0f8f36c8621694b4dc56c8028c667f5d6c2d7497
saalsabl <aisyahnzkk@gmail.com>
2023-10-23 19:49:39 +0700

    modif3

--- jobsheet9/ArrayBilangan01.java
+++ jobsheet9/ArrayBilangan01.java
@@ -3,7 +3,7 @@ package jobsheet9;
 public class ArrayBilangan01 {
     public static void main(String[] args) {
         int[] bil = {12867, 5, 7, 2000000};
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i <= 4; i++){
         System.out.println(bil[i]);
     }
     }
