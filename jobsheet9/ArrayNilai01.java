93d1f378710720932eb4e64700a621f21a9e727d
saalsabl <aisyahnzkk@gmail.com>
2023-10-24 19:03:31 +0700

    modif3

--- jobsheet9/ArrayNilai01.java
+++ jobsheet9/ArrayNilai01.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class ArrayNilai01 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for(int i = 0; i < nilaiAkhir.length; i++){
-            System.out.print("Masukkan nilai akhir ke-"+ i + " : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = scan.nextInt();
         }
-        for (int i = 0; i < nilaiAkhir.length; i++){
-            if (nilaiAkhir[i] > 70){
-                System.out.println("Mahasiswa ke-"+i+"lulus!");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + "lulus!");
+            } else {
+                System.out.println("Mahasiswa ke- " + i + " tidak lulus!");
             }
-           
+
         }
     }
-    
+
 }
