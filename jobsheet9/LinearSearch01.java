3a24df97294fef3168c76929fe0e6d557163c808
saalsabl <aisyahnzkk@gmail.com>
2023-10-24 19:50:35 +0700

    modif1

--- jobsheet9/LinearSearch01.java
+++ jobsheet9/LinearSearch01.java
@@ -1,17 +1,39 @@
 package jobsheet9;
 
+import java.util.Scanner;
+
 public class LinearSearch01 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        
+        Scanner scan = new Scanner (System.in);
 
-        for (int i = 0; i < arrayInt.length; i++){
-            if (arrayInt[i] == key){
+        // Membaca jumlah elemen array dari pengguna
+        System.out.print("Masukkan jumlah elemen array: ");
+        int n = scan.nextInt();
+        int[] arrayInt = new int[n];
+
+        // Membaca elemen-elemen array dari pengguna
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = scan.nextInt();
+        }
+
+        // Membaca key yang ingin dicari
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = scan.nextInt();
+        int hasil = -1;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println("key ada dalam array pada posisi indeks ke-"+hasil);
+
+        if (hasil != -1) {
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan dalam array.");
+        }
     }
 }
