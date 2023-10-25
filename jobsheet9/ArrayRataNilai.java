cfc5423b001ff3f13a77f16d0cd3c15f83bcfa2b
saalsabl <aisyahnzkk@gmail.com>
2023-10-25 07:35:39 +0700

    modif1

--- jobsheet9/ArrayRataNilai.java
+++ jobsheet9/ArrayRataNilai.java
@@ -8,14 +8,22 @@ public class ArrayRataNilai {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int mhsLulus = 0;
+        
         for (int i = 0; i < nilaiMhs.length; i++){
            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
            nilaiMhs[i] = scan.nextInt();
+           if (nilaiMhs[i] > 70) {
+            mhsLulus++;
+            
+           }
         }
         for (int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+
+        System.out.println("Jumlah mahasiswa yang lulus (>70) : " + mhsLulus);
     }
-}
+}       
