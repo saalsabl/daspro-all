0064459d774b03b2c785e2a1ca97b31356cb24a4
saalsabl <aisyahnzkk@gmail.com>
2023-10-30 19:35:42 +0700

    percobaan3 + modif

--- jobsheet10/Numbers01.java
+++ jobsheet10/Numbers01.java
@@ -12,6 +12,7 @@ public class Numbers01 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+        
         for (int i = 0; i < myNumbers.length; i++){
             System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
         }
