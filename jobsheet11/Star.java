de987446a6593c1e4e7b75d3329642eea4a9340b
saalsabl <aisyahnzkk@gmail.com>
2023-11-06 18:58:14 +0700

    modiff percobaan1

--- jobsheet11/Star.java
+++ jobsheet11/Star.java
@@ -5,7 +5,7 @@ public class Star {
     Scanner sc = new Scanner(System.in);
     System.out.print("Masukkan nilai N = ");
     int N = sc.nextInt();
-    for(int i=1; i<=N; i++){
+    for(int i=0; i>N; i--){
         System.out.print("*");
     }  
     }
