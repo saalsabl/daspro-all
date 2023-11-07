bc28f6c8c17d61c086e0a27474611f14cd63b466
saalsabl <aisyahnzkk@gmail.com>
2023-11-08 06:32:53 +0700

    modif penambahan ratarata

--- jobsheet11/NestedLoop_2341720171.java
+++ jobsheet11/NestedLoop_2341720171.java
@@ -1,24 +1,29 @@
 package jobsheet11;
 import java.util.Scanner;
-public class NestedLoop_2341720171{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[][] temps = new int[5][7];
-
-        for (int i = 0; i < temps.length; i++){
-            System.out.println("Kota ke-" + i);
-            for (int j = 0; j < temps[0]. length; j++){
-                System.out.print("Hari ke-" + (j+1) + ": ");
-                temps[i][j] = (int) input.nextDouble();
-            }
-            System.out.println();
+public class NestedLoop_2341720171 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double[][] temps = new double [5][7];
+    
+    for (int i = 0; i < temps.length; i++){
+        System.out.println("Kota ke-" + i);
+        for (int j = 0; j < temps[0].length; j++){
+           System.out.print("Hari ke-" + (j + 1) + ": ");
+           temps[i][j] = sc.nextDouble(); 
         }
-        for (int i = 0; i < temps.length; i++){
-            System.out.print("Kota ke-" + (i+1) + ":");
-            for (int j = 0; j < temps[0].length; j++){
-                System.out.print(temps [i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println();
     }
+    for (int i = 0; i < temps.length; i++){
+        System.out.print("Kota ke-" + (i + 1) + ": ");
+        for (double temperature : temps[i]){
+            System.out.print(temperature + " ");
+        }
+        double total = 0;
+        for (double temperature : temps[i]){
+            total += temperature;
+        }
+        double rataRata = total / temps[i].length;
+        System.out.println("Rata rata : " + rataRata);
+    }
+}   
 }
