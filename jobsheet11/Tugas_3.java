f793f2f52a8f39cff50d1c190c97cf6ad761d6f3
saalsabl <aisyahnzkk@gmail.com>
2023-11-08 07:36:18 +0700

    Tugas individu 1 s.d. 3

---
+++ jobsheet11/Tugas_3.java
@@ -0,0 +1,22 @@
+package jobsheet11;
+import java.util.Scanner;
+public class Tugas_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N (minimal 3) : ");
+        int N = sc.nextInt();
+
+        if (N < 3){
+        }
+        for (int i = 1; i <= N; i++){
+            for (int j =1; j <= N; j++){
+                if (i == 1 || i == N || j == 1 || j == N){
+                    System.out.print(N);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
