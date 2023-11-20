e912d12fd7dc98275fe3feec1899d886a4b7c593
saalsabl <aisyahnzkk@gmail.com>
2023-11-20 11:54:31 +0700

    Percobaan1

---
+++ jobsheet13/Terimaksih_01.java
@@ -0,0 +1,8 @@
+public class Terimaksih_01 {
+    public static void UcapanTerimakasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like I could ask you anything.");
+        }
+         public static void main(String[] args){
+            UcapanTerimakasih();
+    }
+}
