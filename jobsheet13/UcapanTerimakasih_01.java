9f3a0bec32961f48648ad5fa9aa602fd251372dc
saalsabl <aisyahnzkk@gmail.com>
2023-11-22 08:55:06 +0700

    Percobaan2

--- jobsheet13/UcapanTerimakasih_01.java
+++ jobsheet13/UcapanTerimakasih_01.java
@@ -8,11 +8,16 @@ public class UcapanTerimakasih_01 {
         sc.close();
         return namaOrang;
     }
+    public static void UcapanTambahan(String tambahan) {
+        System.out.println("Pesan tambahan: "+tambahan);
+    }
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        UcapanTambahan("You've made a lasting impact on my life. Thank you!");
     }
 }
+
