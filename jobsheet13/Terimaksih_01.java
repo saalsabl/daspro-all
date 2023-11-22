7c954aabb9d7bcfd8bd6137b4bfae0f4904c799a
saalsabl <aisyahnzkk@gmail.com>
2023-11-22 08:54:44 +0700

    Percobaan1 + modif

--- jobsheet13/Terimaksih_01.java
+++ jobsheet13/Terimaksih_01.java
@@ -1,7 +1,14 @@
 public class Terimaksih_01 {
-         public static void main(String[] args){
+         public static void Ucapanterimakasih(){
             System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
-
+        }
+        public static void UcapanTambahan(String ucapan){
+            System.out.println(ucapan);
+        }
+        public static void main(String[] args) {
+            Ucapanterimakasih();
+            String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+            UcapanTambahan(ucapan);
         }
     }
 
