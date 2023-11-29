713e695a29e7017ba3fbb117cd180d63cae835e0
saalsabl <aisyahnzkk@gmail.com>
2023-11-29 07:20:49 +0700

    modif

--- jobsheet14/Percobaan2.java
+++ jobsheet14/Percobaan2.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat (int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
             return (x * hitungPangkat(x, y - 1));
-        }  
+        }
+    }
+    static void cetakDeretPangkat(int x, int y){
+        if (y == 0){
+            System.out.println("1");
+        } else {
+            System.out.print(x + "x");
+            cetakDeretPangkat(x, y - 1);
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,6 +25,10 @@ public class Percobaan2 {
         System.out.print("Pangkat : ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.println("Hasil perpangkatan : " + hitungPangkat(bilangan, pangkat));   
+
+        cetakDeretPangkat(bilangan, pangkat);
     }
+            
 }
+    
